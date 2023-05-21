0eafd2b345a8da8b408bfaa6a5a9db6f74619b02
abdifatah basher <ab.sav@icloud.com>
2023-05-21 22:27:47 +0100

    Create test project

---
+++ Tests/Application/API/UserProfile/Commands/Create/CreateUserProfileCommandHandlerTests.cs
@@ -0,0 +1,14 @@
+namespace Tests.Application.API.UserProfile.Commands.Create;
+
+public class CreateUserProfileCommandHandlerTests
+{
+    [Fact]
+    public void Handle_Should_ReturnFailureResult_WhenRequestIsEmpty()
+    {
+        // Arrange
+        
+        // Act
+        
+        // Assert
+    }
+}
