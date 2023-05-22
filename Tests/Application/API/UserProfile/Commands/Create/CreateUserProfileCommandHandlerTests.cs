66e9dac26f51fb9e06b972e38e105e6ec61ec9b5
abdifatah basher <ab.sav@icloud.com>
2023-05-22 22:31:59 +0100

    Add unit tests for CreateUserProfileCommandHandler

--- Tests/Application/API/UserProfile/Commands/Create/CreateUserProfileCommandHandlerTests.cs
+++ Tests/Application/API/UserProfile/Commands/Create/CreateUserProfileCommandHandlerTests.cs
@@ -1,14 +1,65 @@
+using Application.API.V1.UserProfiles;
+using Application.API.V1.UserProfiles.Commands.Create;
+using Application.API.V1.UserProfiles.Models;
+using Common.Enums;
+using Moq;
+
 namespace Tests.Application.API.UserProfile.Commands.Create;
 
 public class CreateUserProfileCommandHandlerTests
 {
+    private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
+
+    public CreateUserProfileCommandHandlerTests()
+    {
+        _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
+    }
+
     [Fact]
-    public void Handle_Should_ReturnFailureResult_WhenRequestIsEmpty()
+    public async Task Handle_Should_ReturnArgumentNullException_WhenRequestIsNull()
     {
         // Arrange
+        var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
         
+        // Act and Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+        {
+            await handler.Handle(null, default);
+        });
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnCreateUserProfileDto_WhenValidRequest()
+    {
+        // Arrange
+        var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
+
+        var command = new CreateUserProfileCommand()
+        {
+            Id = new Guid(),
+            Age = 27,
+            Gender = Gender.Male,
+            Height = 175,
+            Weight = 128
+        };
+
+        var createUserProfileDto = new CreateUserProfileDto()
+        {
+            Id = new Guid(),
+            Age = 27,
+            Gender = Gender.Male,
+            Height = 175,
+            Weight = 128
+        };
+
+        _userProfileRepositoryMock.Setup(
+                x => x.CreateUserProfile(command, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(createUserProfileDto);
+
         // Act
-        
+        var result = await handler.Handle(command, default);
+
         // Assert
+        Assert.Equal(createUserProfileDto, result);
     }
 }
