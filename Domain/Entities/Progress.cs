3ed066883b1005c84136f4e99bfded033a47da25
abdifatah basher <ab.sav@icloud.com>
2023-05-28 16:18:40 +0100

    Add correct navigational properties between entities

--- Domain/Entities/Progress.cs
+++ Domain/Entities/Progress.cs
@@ -1,10 +1,10 @@
 namespace Domain.Entities;
 
-public class Progress : TableAudit
+public class Progress : BaseEntity
 {
-    public Guid Id { get; set; }
-
+    public Guid UserId { get; set; }
+    public Guid GoalId { get; set; }
     public decimal Value { get; set; }
-
+    public User User { get; set; }
     public Goal Goal { get; set; }
 }
