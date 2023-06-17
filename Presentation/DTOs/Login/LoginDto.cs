2f5283ab1cb9b8ef09a430acbaf45a0291553bcd
abasher423 <56160528+abasher423@users.noreply.github.com>
2023-06-17 19:25:13 +0100

    Create login controller

---
+++ Presentation/DTOs/Login/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace HealthTracker.DTOs.Login;
+
+public class LoginDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Token { get; set; }
+}
---
+++ Presentation/Mappings/LoginMappingProfile.cs
@@ -0,0 +1,20 @@
+using Application.API.V1.Login.Commands;
+using Application.API.V1.Login.Models;
+using Application.API.V1.Register.Commands;
+using Application.API.V1.Register.Models;
+using AutoMapper;
+using HealthTracker.DTOs.Login;
+
+namespace HealthTracker.Mappings;
+
+public class LoginMappingProfile : Profile
+{
+    public LoginMappingProfile()
+    {
+        CreateMap<LoginCommand, LoginRequest>().ReverseMap();
+        CreateMap<RegisterCommand, RegisterRequest>().ReverseMap();
+
+        CreateMap<LoginModel, LoginDto>().ReverseMap();
+        CreateMap<RegisterModel, LoginDto>().ReverseMap();
+    }
+}
