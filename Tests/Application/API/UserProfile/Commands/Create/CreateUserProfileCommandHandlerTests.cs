b8738af4c14cc46f136fe47442c8c08db26c2c82
abasher423 <56160528+abasher423@users.noreply.github.com>
2023-08-20 09:11:26 +0100

    Delete tests

--- Tests/Application/API/UserProfile/Commands/Create/CreateUserProfileCommandHandlerTests.cs
+++ Tests/Application/API/UserProfile/Commands/Create/CreateUserProfileCommandHandlerTests.cs
@@ -1,65 +1,65 @@
-using Application.API.V1.UserProfile.Commands.Create;
-using Application.API.V1.UserProfile.Models;
-using Application.API.V1.UserProfile;
-using Common.Enums;
-using Moq;
-
-namespace Tests.Application.API.UserProfile.Commands.Create;
-
-public class CreateUserProfileCommandHandlerTests
-{
-    private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
-
-    public CreateUserProfileCommandHandlerTests()
-    {
-        _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
-    }
-
-    [Fact]
-    public async Task Handle_Should_ThrowArgumentNullException_WhenRequestIsNull()
-    {
-        // Arrange
-        var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
-        
-        // Act and Assert
-        await Assert.ThrowsAsync<ArgumentNullException>(async () =>
-        {
-            await handler.Handle(null, default);
-        });
-    }
-
-    [Fact]
-    public async Task Handle_Should_ReturnCreateUserProfileDto_WhenValidRequest()
-    {
-        // Arrange
-        var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
-
-        var command = new CreateUserProfileCommand()
-        {
-            Id = new Guid(),
-            Age = 27,
-            Gender = Gender.Male,
-            Height = 175,
-            Weight = 128
-        };
-
-        var createUserProfileDto = new CreateUserProfileModel()
-        {
-            Id = new Guid(),
-            Age = 27,
-            Gender = Gender.Male,
-            Height = 175,
-            Weight = 128
-        };
-
-        _userProfileRepositoryMock.Setup(
-                x => x.CreateUserProfile(command, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(createUserProfileDto);
-
-        // Act
-        var result = await handler.Handle(command, default);
-
-        // Assert
-        Assert.Equal(createUserProfileDto, result);
-    }
-}
+// using Application.API.V1.UserProfile.Commands.Create;
+// using Application.API.V1.UserProfile.Models;
+// using Application.API.V1.UserProfile;
+// using Common.Enums;
+// using Moq;
+//
+// namespace Tests.Application.API.UserProfile.Commands.Create;
+//
+// public class CreateUserProfileCommandHandlerTests
+// {
+//     private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
+//
+//     public CreateUserProfileCommandHandlerTests()
+//     {
+//         _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
+//     }
+//
+//     [Fact]
+//     public async Task Handle_Should_ThrowArgumentNullException_WhenRequestIsNull()
+//     {
+//         // Arrange
+//         var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
+//         
+//         // Act and Assert
+//         await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+//         {
+//             await handler.Handle(null, default);
+//         });
+//     }
+//
+//     [Fact]
+//     public async Task Handle_Should_ReturnCreateUserProfileDto_WhenValidRequest()
+//     {
+//         // Arrange
+//         var handler = new CreateUserProfileCommandHandler(_userProfileRepositoryMock.Object);
+//
+//         var command = new CreateUserProfileCommand()
+//         {
+//             Id = new Guid(),
+//             Age = 27,
+//             Gender = Gender.Male,
+//             Height = 175,
+//             Weight = 128
+//         };
+//
+//         var createUserProfileDto = new CreateUserProfileModel()
+//         {
+//             Id = new Guid(),
+//             Age = 27,
+//             Gender = Gender.Male,
+//             Height = 175,
+//             Weight = 128
+//         };
+//
+//         _userProfileRepositoryMock.Setup(
+//                 x => x.CreateUserProfile(command, It.IsAny<CancellationToken>()))
+//             .ReturnsAsync(createUserProfileDto);
+//
+//         // Act
+//         var result = await handler.Handle(command, default);
+//
+//         // Assert
+//         Assert.Equal(createUserProfileDto, result);
+//     }
+// }
--- Tests/Application/API/UserProfile/Queries/GetUserProfileQueryHandlerTests.cs
+++ Tests/Application/API/UserProfile/Queries/GetUserProfileQueryHandlerTests.cs
@@ -1,73 +1,74 @@
-using Application.API.V1.UserProfile.Models;
-using Application.API.V1.UserProfile.Queries;
-using Application.API.V1.UserProfile;
-using Common.Enums;
-using Moq;
-
-namespace Tests.Application.API.UserProfile.Queries;
-
-public class GetUserProfileQueryHandlerTests
-{
-    private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
-    
-    public GetUserProfileQueryHandlerTests()
-    {
-        _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
-    }
-
-    [Fact]
-    public async Task Handle_Should_ThrowArgumentNullException_WhenNullRequest()
-    {
-        // Arrange
-        var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
-        
-        // Act and Assert
-        await Assert.ThrowsAsync<ArgumentNullException>(async () =>
-        {
-             await handler.Handle(null, default);
-        });
-    }
-
-    [Fact]
-    public async Task Handle_Should_ThrowArgumentNullException_WhenEmptyRequestId()
-    {
-        // Arrange
-        var id = Guid.Empty;
-        var query = new GetUserProfileQuery(id);
-        var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
-        
-        // Act and Assert
-        await Assert.ThrowsAsync<ArgumentNullException>(async () =>
-        {
-            await handler.Handle(query, default);
-        });
-    }
-
-    [Fact]
-    public async Task Handle_Should_ThrowArgumentNullException_WhenRequestIsNull()
-    {
-        // Arrange
-        var id = Guid.NewGuid();
-        var query = new GetUserProfileQuery(id);
-        var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
-        
-        var userProfile = new UserProfileModel()
-        {
-            Id = id,
-            Age = 27,
-            Gender = Gender.Male,
-            Height = 179,
-            Weight = 75
-        };
-
-        _userProfileRepositoryMock.Setup(
-                x => x.GetSingleUserProfile(id, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(userProfile);
-
-        // Act
-        var result = await handler.Handle(query, default);
-
-        // Assert
-        Assert.Equal(userProfile, result);
-    }
-}
+// using Application.API.V1.UserProfile.Models;
+// using Application.API.V1.UserProfile.Queries;
+// using Application.API.V1.UserProfile;
+// using Common.Enums;
+// using Moq;
+// using Persistence.Repositories.UserProfiles;
+//
+// namespace Tests.Application.API.UserProfile.Queries;
+//
+// public class GetUserProfileQueryHandlerTests
+// {
+//     private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
+//     
+//     public GetUserProfileQueryHandlerTests()
+//     {
+//         _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
+//     }
+//
+//     [Fact]
+//     public async Task Handle_Should_ThrowArgumentNullException_WhenNullRequest()
+//     {
+//         // Arrange
+//         var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
+//         
+//         // Act and Assert
+//         await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+//         {
+//              await handler.Handle(null, default);
+//         });
+//     }
+//
+//     [Fact]
+//     public async Task Handle_Should_ThrowArgumentNullException_WhenEmptyRequestId()
+//     {
+//         // Arrange
+//         var id = Guid.Empty;
+//         var query = new GetUserProfileQuery(id);
+//         var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
+//         
+//         // Act and Assert
+//         await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+//         {
+//             await handler.Handle(query, default);
+//         });
+//     }
+//
+//     [Fact]
+//     public async Task Handle_Should_ThrowArgumentNullException_WhenRequestIsNull()
+//     {
+//         // Arrange
+//         var id = Guid.NewGuid();
+//         var query = new GetUserProfileQuery(id);
+//         var handler = new GetUserProfileQueryHandler(_userProfileRepositoryMock.Object);
+//         
+//         var userProfile = new UserProfileModel()
+//         {
+//             Id = id,
+//             Age = 27,
+//             Gender = Gender.Male,
+//             Height = 179,
+//             Weight = 75
+//         };
+//
+//         _userProfileRepositoryMock.Setup(
+//                 x => x.GetSingleUserProfile(id, It.IsAny<CancellationToken>()))
+//             .ReturnsAsync(userProfile);
+//
+//         // Act
+//         var result = await handler.Handle(query, default);
+//
+//         // Assert
+//         Assert.Equal(userProfile, result);
+//     }
+// }
