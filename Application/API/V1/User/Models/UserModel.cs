64b6b003a6c662d3466a865f4bf2dcb76fc14ddc
abasher423 <56160528+abasher423@users.noreply.github.com>
2023-08-20 09:10:36 +0100

    Remove hashed password from response object

--- Application/API/V1/User/Models/UserModel.cs
+++ Application/API/V1/User/Models/UserModel.cs
@@ -4,7 +4,6 @@ public class UserModel
 {
     public Guid Id { get; set; }
     public string Email { get; set; }
-    public string HashedPassword { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
 }
