04e641663f2143dac3bf1244f065ba25abeae226
abasher423 <56160528+abasher423@users.noreply.github.com>
2023-09-10 17:07:36 +0100

    Add validation to health data entry entity

--- Persistence/Configurations/EntityConfigurations/HealthDataEntryEntityBuilder.cs
+++ Persistence/Configurations/EntityConfigurations/HealthDataEntryEntityBuilder.cs
@@ -1,3 +1,4 @@
+using Common.Enums;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -12,7 +13,12 @@ public class HealthDataEntryEntityBuilder : IEntityTypeConfiguration<HealthDataE
         builder.HasKey(x => x.Id).HasName("pk_health_data_entry_id");
 
         builder.Property(x => x.Id).IsRequired().HasColumnName("id");
-        builder.Property(x => x.Name).IsRequired().HasColumnName("name");
+        
+        builder.Property(x => x.Name).IsRequired().HasColumnName("name")
+            .HasConversion(v => v.ToString(), // Convert enum to string for storage
+            v => (HealthDataName)Enum.Parse(typeof(HealthDataName), v) // Convert string to enum when reading from the database
+        ).HasMaxLength(30);
+        
         builder.Property(x => x.Value).IsRequired().HasColumnName("value");
 
         builder.HasOne<User>(hde => hde.User)
